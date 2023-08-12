63a4f707eae674f61d98e39bd156f419db0045f2
Justin Kokolo <justinkokolo5@gmail.com>
2023-08-12 12:54:30 +0200

    first commit

--- TaskApi/Controllers/WeatherForecastController.cs
+++ TaskApi/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TaskApi.Controllers
 {
+    [AllowAnonymous]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
